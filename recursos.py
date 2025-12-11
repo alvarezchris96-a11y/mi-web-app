fb198fff103fb89ebb71f3319a287d140603374f
Chris Alv <alvarezchris96@gmail.com>
2025-12-11 14:03:31 -0600

    Se agregaron los recursos

---
+++ recursos.py
@@ -0,0 +1,26 @@
+from flask_restful import Resource
+
+from flask import make_response, render_template
+# Vamos a crear Recursos - Para nosotros poder crear recursos
+# lo haremos a través de clases y objetos
+class HolaMundo(Resource):
+    # Los recursos van a poder ejecutar dos acciones -> métodos
+    def get(self):
+        return {'hola': 'hola mundo'}
+
+# Será un recurso
+class PantallaInicio(Resource):
+    def get(self):
+        #renderisamos el contenido del archivo html
+
+        contenido = render_template('index.html')
+
+        # retornamos el contenido renderizado en una respuesta
+        return make_response(contenido)
+
+
+class Despedida(Resource):
+    def get(self):
+
+        print('Esto es un texto de prueba')
+        return 'Adios'
